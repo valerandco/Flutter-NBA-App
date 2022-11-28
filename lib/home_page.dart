7e09f4a80b7e539571324c745f0e4995b5312c28
Valerii Gassiev <vvv.gassiev@gmail.com>
2022-11-28 10:51:41 +0300

    add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,9 @@ import 'package:http/http.dart' as http;
 import 'model/team.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  List<Team> teams = [];
 
   Future getTeams() async {
     var response = await http.get(Uri.https('balldontlie.io','api/v1/teams'));
@@ -17,13 +19,30 @@ class HomePage extends StatelessWidget {
         abbreviation: eachTeam['abbreviation'],
         city: eachTeam['city'],
       );
+      teams.add(team);
     }
+    
+    print(teams.length);
   }
 
   @override
   Widget build(BuildContext context) {
     getTeams();
     return Scaffold(
+      body: FutureBuilder(
+        future: getTeams(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done){
+            return ListView.builder(
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(teams[index].abbreviation),
+                );
+              },
+            );
+          }
+        }
+      ),
 
     );
   }
