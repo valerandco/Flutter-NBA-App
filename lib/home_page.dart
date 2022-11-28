1d967a57997c27aeaf5d4c3ba3dd80690686f826
Valerii Gassiev <vvv.gassiev@gmail.com>
2022-11-28 10:33:28 +0300

    API

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Future getTeams() async {
+    var response = await http.get(Uri.https('balldontlie.io','api/v1/teams'));
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+
+    );
   }
 }
